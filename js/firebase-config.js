6b3489405e86f66859955dcfdbc2f6a704f6fd17
BenPomme <benjamin.pommeraud@gmail.com>
2025-04-08 11:12:22 +0200

    Update Firebase configuration to use degenlander project

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyDZaU-AnRxHIjA5WgXBFXQG4P4VwOLj8Bs",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "degenlander-3b1c7.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID || "degenlander-3b1c7",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "degenlander-3b1c7.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "431855620245",
-  appId: process.env.FIREBASE_APP_ID || "1:431855620245:web:80b5c8da3b688972cc5ce1"
+  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyCol18DWGnK9NgxDK2h7YwTOGGegNSlJGM",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "degenlander.firebaseapp.com",
+  projectId: process.env.FIREBASE_PROJECT_ID || "degenlander",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "degenlander.firebasestorage.app",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "520104814068",
+  appId: process.env.FIREBASE_APP_ID || "1:520104814068:web:2c364bc0d0dcc0a3fa05a1",
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID || "G-NH7W5BZST2"
 };
 
 // For Github Pages hosting, we need to include the fallback values
